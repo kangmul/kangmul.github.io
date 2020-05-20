398486c9aa839579f3873165166acd7dacb2ff63
kangmul <muluk.dharmayana@gmail.com>
2020-05-20 17:56:54 +0700

    merapihkan dan menambahkan feature image dan logo

--- assets/script/myscript.js
+++ assets/script/myscript.js
@@ -1,27 +1,44 @@
-$(document).ready(function () {
+(function ($) {
+    let jarakContent = $(document).scrollTop();
+    if (jarakContent > 50) {
+        $('.navbar').removeClass('bg-transparent').addClass('bg-dark')
+    }
 
-    let hrefTujuan = $(this).attr("href");
-    let tujuan = $(hrefTujuan);
-    $(".nav-link").on("click", function () {
-       
+    $(".nav-link").on("click", function (e) {
         let hrefTujuan = $(this).attr("href");
         let tujuan = $(hrefTujuan);
-
-        console.log(tujuan);
-    
-      $('html body').animate({
-          scroll: tujuan.offset().top,
-        }, 2000, 'easeInOutExpo'); 
-
-        //  event.preventDefault();
+        $('html, body').animate({
+            scrollTop: tujuan.offset().top,
+            // setInterval: 500,
+        }, 800);
+        e.preventDefault();
     });
-    
-    // console.log(tujuan.offset())
 
-    // $('body').on('scroll', function (e) {
-    //     console.log(e)
-    // });
-    
-});
+    $(document).on('scroll', function () {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 50) {
+            $('.navbar-expand-lg').removeClass('bg-transparent').addClass('bg-dark', 400, 'swing');
+        } else {
+            $('.navbar-expand-lg').removeClass('bg-dark').addClass('bg-transparent', 400, 'swing');
+        }
+
+        // // parallax
+        // $(' ')
+
+    });
+
+    $('.nav-link').on('click', function () {
+        $('.collapse').collapse('hide');
+    });
+
+    // parallax
+    $('windows').on('scroll', function () {
+        let winScroll
+    })
+
+
+})(jQuery);
+
+
 
 
